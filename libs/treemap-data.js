3b5a3b1076939a90b2cee1b514ea2500fec7eac1
James Liu <james.liu668@aliyun.com>
2020-06-02 14:14:52 +0800

    add treemap example

---
+++ libs/treemap-data.js
@@ -0,0 +1,30 @@
+var data = [
+    {
+        namqqe : '舆情类',
+        value: 5118
+    },
+    {
+        name: '政策类',
+        value: 767
+    },
+    {
+        name: '搜索类',
+        value: 328
+    },
+    {
+        name: '知识问答类',
+        value: 542
+    },
+    {
+        name: '社交类',
+        value: 430
+    },
+    {
+        name: '短视频',
+        value: 60
+    },
+    {
+        name: '电商类',
+        value: 130
+    }
+]
